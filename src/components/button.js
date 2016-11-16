3b7d8da491939bb6571d51c03d33f7f10f721c35
Patrick Mounts <patrickmnts@gmail.com>
2016-11-16 08:28:05 -0800

    inital commit of forked react toolkit

---
+++ src/components/button.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  render() {
+    return (
+      <button>{this.props.text}</button>
+    );
+  }
+}
+
+Button.defaultProps = {text: 'Default'};
+
+export default Button;
